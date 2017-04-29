e83dd192029be2fccaf50599e148f84630c054a2
Joey Kirby <jkirbymusic@gmail.com>
2017-04-29 13:21:58 -0500

    nav ticket btn to ticketfly

--- js/creative.js
+++ js/creative.js
@@ -61,4 +61,18 @@
         }
     });
 
+    $(document).ready(function(){       
+       var scroll_start = 0;
+       var startchange = $('#about');
+       var offset = startchange.offset();
+       $(document).scroll(function() { 
+          scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top) {
+              $('#mainNav').css('background-color', 'lightblue');
+           } else {
+              $('#mainNav').css('background-color', 'black');
+           }
+       });
+    });
+
 })(jQuery); // End of use strict
