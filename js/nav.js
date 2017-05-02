9bb96c8ba2f63c1460e75423248cd4ce015426ce
Joey Kirby <jkirbymusic@gmail.com>
2017-05-02 17:57:46 -0500

    nav active class

--- js/nav.js
+++ js/nav.js
@@ -11,3 +11,12 @@ $(document).ready(function(){
        }
    });
 });
+
+$('.navbar li').click(function(e) {
+    $('.navbar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+    // e.preventDefault();
+});
