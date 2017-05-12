b4bfd921938e2161ad31ffc8c478b1ceed35bf02
Joey Kirby <jkirbymusic@gmail.com>
2017-05-12 11:08:07 -0500

    nav with logo

--- js/nav.js
+++ js/nav.js
@@ -7,8 +7,10 @@ $(document).ready(function(){
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
           $('#mainNav').css('background-color', '#252525');
+          $('#deep-nav').css('background-color', '#252525');
        } else {
           $('#mainNav').css('background-color', 'black');
+          $('#deep-nav').css('background-color', 'black');
        }
    });
 });
@@ -26,3 +28,37 @@ $(this).find('.dropdown-menu').first().stop(true, true).delay(350).slideUp();
 setTimeout(function() {
     $('#myModal').modal();
 }, 13000);
+
+// CAROUSEL
+
+// $(document).ready(function(){
+//   var clickEvent = false;
+//   $('#myCarousel').carousel({
+//     interval:   4000  
+//   }).on('click', '.list-group li', function() {
+//       clickEvent = true;
+//       $('.list-group li').removeClass('active');
+//       $(this).addClass('active');   
+//   }).on('slid.bs.carousel', function(e) {
+//     if(!clickEvent) {
+//       var count = $('.list-group').children().length -1;
+//       var current = $('.list-group li.active');
+//       current.removeClass('active').next().addClass('active');
+//       var id = parseInt(current.data('slide-to'));
+//       if(count == id) {
+//         $('.list-group li').first().addClass('active'); 
+//       }
+//     }
+//     clickEvent = false;
+//   });
+// });
+// $(window).load(function() {
+//     var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+//     var itemlength = $('#myCarousel .item').length;
+//   var triggerheight = Math.round(boxheight/itemlength+1);
+//   $('#myCarousel .list-group-item').outerHeight(triggerheight);
+// });
+
+
+
+
