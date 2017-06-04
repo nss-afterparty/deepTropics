ec921f15b02ed9baf4fd0c65e8527f448d2711e2
Joey Kirby <jkirbymusic@gmail.com>
2017-06-04 13:07:09 -0500

    Update lineup.js

--- js/lineup.js
+++ js/lineup.js
@@ -3,7 +3,7 @@
 console.log('HEY HEY HEY');
 
 
-$.getJSON( "/lineup.json", function( data ) {
+$.getJSON( "lineup.json", function( data ) {
     console.log('data', data);
 
   $.each( data, function( key, val ) {
