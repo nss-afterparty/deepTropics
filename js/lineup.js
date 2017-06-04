62d367fa4c32250565a96a86b02ea9fa58856a72
Joey Kirby <jkirbymusic@gmail.com>
2017-06-04 03:20:03 -0500

    lineup positioning

--- js/lineup.js
+++ js/lineup.js
@@ -2,36 +2,22 @@
 (function() {
 console.log('HEY HEY HEY');
 
-$.getJSON( "/../lineup.json", function( data ) {
+$.getJSON( "../lineup.json", function( data ) {
     console.log('data', data);
 
   $.each( data, function( key, val ) {
     console.log('value', val);
-    $('#artist-container').append("<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3 portfolio-item' id=" + key + ">" + "<a href='#'>" + "<img class='img-responsive artist-img'" + "src='" + val.image + "'" + "alt='" + val.artist + "'>" + "</a>" + "</li>");
+    let artistId = val.artist;
+    let artistImg = val.image
+
+    $('#artist-container').append("<div class='card card-inverse col-sm-4 col-xs-6 col-md-3 col-lg-3'>" + "<div class='card' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + val.artist + "</h2>" + "</div>" + "</div>" + "</div>");
+
+    $('#'+key).css('background-image', 'url(' + artistImg + ')');
   });
 });
 
 })();
 
-// <div class="row">
-//   <div class="col-sm-4 col-xs-6 col-md-3 col-lg-3 portfolio-item">
-//     <a href="#">
-//       <img style="width: 450px" class="img-responsive" src="lineup_img/Gramatik.jpg" alt="Gramatik">
-//     </a>
-//   </div>
-//   <div class="col-sm-4 col-xs-6 col-md-3 col-lg-3 portfolio-item">
-//     <a href="#">
-//       <img class="img-responsive" src="lineup_img/Bob_Moses.jpg" alt="Bob Moses">
-//     </a>
-//   </div>
-//   <div class="col-sm-4 col-xs-6 col-md-3 col-lg-3 portfolio-item">
-//     <a href="#">
-//       <img class="img-responsive" src="lineup_img/Paper_Diamond.jpg" alt="Paper Diamond">
-//     </a>
-//   </div>
-//   <div class="col-sm-4 col-xs-6 col-md-3 col-lg-3 portfolio-item">
-//     <a href="#">
-//       <img class="img-responsive" src="lineup_img/Troyboi.jpg" alt="Troyboi">
-//     </a>
-//   </div>
-// </div>
+
+// artist-card card transparent
+
