e4ca67d8204a37c812a99d86986464ff37d006bd
Joey Kirby <jkirbymusic@gmail.com>
2017-06-04 22:39:35 -0500

    ticketfly and shop links

--- js/lineup.js
+++ js/lineup.js
@@ -10,11 +10,12 @@ $.getJSON( "/lineup.json", function( data ) {
     let artistId = val.artist.toUpperCase();
     let artistImg = val.image;
 
-    $('#artist-container').append("<div class='card card-inverse col-sm-4 col-xs-6 col-md-3 col-lg-3'>" + "<div class='card' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + artistId + "</h2>" + "</div>" + "</div>" + "</div>");
+    $('#artist-container').append("<div class='card card-inverse col-sm-4 col-xs-6 col-md-3 col-lg-3'>" + "<a href='#' title=" + "'" + key + "'>" + "<div class='card artist-thumbnail' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + artistId + "</h2>" + "</div>" + "</div>" + "</a>" + "</div>");
 
     $('#'+key).css('background-image', 'url(' + artistImg + ')');
   });
 });
 
 })();
+
 
