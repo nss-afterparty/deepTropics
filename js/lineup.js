19752bad7c0457ba7a4082d4de24f476e29b743d
Joey Kirby <jkirbymusic@gmail.com>
2017-06-05 01:55:59 -0500

    modal w img

--- js/lineup.js
+++ js/lineup.js
@@ -10,17 +10,17 @@ $.getJSON( "/lineup.json", function( data ) {
     let artistName = val.artist.toUpperCase();
     let artistImg = val.image;
     let artistBio = val.bio;
-    createModal(key,artistName, artistImg, artistBio);
+    createModal(key, artistName, artistImg, artistBio);
 
 
-    $('#artist-container').append("<div type='button' " +  "data-toggle='modal'" + "data-target='a" + key + "'" + "class='card card-inverse btn col-sm-4 artist-thumbnail col-xs-6 col-md-3 col-lg-3'>" + "<div class='card' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + artistName + "</h2>" + "</div>" + "</div>" + "</div>");
+    $('#artist-container').append("<div type='button' " +  "data-toggle='modal'" + "data-target='a" + key + "'" + "class='card card-inverse col-sm-4 artist-thumbnail col-xs-6 col-md-3 col-lg-3'>" + "<div class='card' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + artistName + "</h2>" + "</div>" + "</div>" + "</div>");
 
     $('#'+key).css('background-image', 'url(' + artistImg + ')');
   });
 });
 
 //=================================[ Modal Creation ]====================================//
-let createModal = function(key, artistName, artistBio, artistImg ) {
+let createModal = function(key, artistName, artistImg, artistBio) {
     // console.log('name, artistBio, image', key, artistName, artistBio, artistImg);
 
     let html = '<div class="modal fade"' + 'id="a' + key + '"' + 'tabindex="-1" role="dialog" aria-labelledby="' + artistName + '" aria-hidden="true">';
@@ -31,7 +31,8 @@ let createModal = function(key, artistName, artistBio, artistImg ) {
     html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
     html += '<span aria-hidden="true">&times;</span>';
     html += '</button>';
-    html += '</div>';
+    html += '</div>'; //header
+    html += '<img src="' + artistImg + '"' + '>';
     html += '<div class="modal-body">';
     html += artistBio;
     html += '</div>';
