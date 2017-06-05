d14719ba02d8082629f82fbb2c84bad99fac61ef
Joey Kirby <jkirbymusic@gmail.com>
2017-06-05 05:37:36 -0500

    about and additional lineup photos

--- js/lineup.js
+++ js/lineup.js
@@ -10,7 +10,7 @@ $.getJSON( "/lineup.json", function( data ) {
     let artistName = val.artist.toUpperCase();
     let artistImg = val.image;
     let artistBio = val.bio;
-    createModal(key, artistName, artistImg, artistBio);
+    createModal(key, artistName, artistImg, artistBio, val);
 
 
     $('#artist-container').append("<div type='button' " +  "data-toggle='modal'" + "data-target='a" + key + "'" + "class='card card-inverse col-sm-4 artist-thumbnail col-xs-6 col-md-3 col-lg-3'>" + "<div class='card' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + artistName + "</h2>" + "</div>" + "</div>" + "</div>");
@@ -20,9 +20,10 @@ $.getJSON( "/lineup.json", function( data ) {
 });
 
 //=================================[ Modal Creation ]====================================//
-let createModal = function(key, artistName, artistImg, artistBio) {
+let createModal = function(key, artistName, artistImg, artistBio, val) {
     // console.log('name, artistBio, image', key, artistName, artistBio, artistImg);
 
+    //====================[ ARTIST MODAL ]=========================//
     let html = '<div class="modal fade"' + 'id="a' + key + '"' + 'tabindex="-1" role="dialog" aria-labelledby="' + artistName + '" aria-hidden="true">';
     html += '<div class="modal-dialog modal-s" role="document">';
     html += '<div class="modal-content artist-content">';
@@ -33,6 +34,7 @@ let createModal = function(key, artistName, artistImg, artistBio) {
     // html += '</button>';
     // html += '</div>'; //header END
 
+    //=======================[ BODY ]=========================//
     html += '<div class="artist-modal-image">' //image container
     // X CLOSE BTN
     html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
@@ -48,7 +50,24 @@ let createModal = function(key, artistName, artistImg, artistBio) {
     // html += '</div>';
     html += artistBio;
     html += '</div>';
-    html += '<div class="modal-footer">'; 
+
+    //==========[ FOOTER ]==============//
+    html += '<div id="artist-modal-footer" class="modal-footer">'; 
+    if (val.website) {
+        html += '<a target="_blank" href="' + val.website + '"><i class="fa fa-desktop fa-2x modal-social-icons" aria-hidden="true"></i></a>';  
+    }
+    if (val.facebook) {
+        html += '<a target="_blank" href="' + val.facebook + '"><i class="fa fa-facebook-official fa-2x modal-social-icons" aria-hidden="true"></i></a>';
+    }
+    if(val.soundcloud){
+        html += '<a target="_blank" href="' + val.soundcloud + '"><i class="fa fa-soundcloud fa-2x modal-social-icons" aria-hidden="true"></i></a>';
+    }
+    if (val.twitter) {
+        html += '<a target="_blank" href="' + val.twitter + '"><i class="fa fa-twitter fa-2x modal-social-icons" aria-hidden="true"></i></a>';
+    }
+    if(val.spotify){
+        html += '<a target="_blank" href="' + val.spotify + '"><i class="fa fa-spotify fa-2x modal-social-icons" aria-hidden="true"></i></a>';
+    }
     html += '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'; 
     html += '</div>'; //footer
     html += '</div>'; //content
