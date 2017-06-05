6e73d24d171cff71d51796fb1c2baec2b93d9ea1
Joey Kirby <jkirbymusic@gmail.com>
2017-06-05 01:47:23 -0500

    artist modal

--- js/lineup.js
+++ js/lineup.js
@@ -1,19 +1,53 @@
 "use strict";
 (function() {
-console.log('HEY HEY HEY');
 
+//=================================[ Lineup Card Creation ]====================================//
 $.getJSON( "/lineup.json", function( data ) {
     console.log('data', data);
 
   $.each( data, function( key, val ) {
     console.log('value', val);
-    let artistId = val.artist.toUpperCase();
+    let artistName = val.artist.toUpperCase();
     let artistImg = val.image;
+    let artistBio = val.bio;
+    createModal(key,artistName, artistImg, artistBio);
 
-    $('#artist-container').append("<div class='card card-inverse col-sm-4 col-xs-6 col-md-3 col-lg-3'>" + "<a href='#' title=" + "'" + key + "'>" + "<div class='card artist-thumbnail' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + artistId + "</h2>" + "</div>" + "</div>" + "</a>" + "</div>");
+
+    $('#artist-container').append("<div type='button' " +  "data-toggle='modal'" + "data-target='a" + key + "'" + "class='card card-inverse btn col-sm-4 artist-thumbnail col-xs-6 col-md-3 col-lg-3'>" + "<div class='card' id='" + key + "'>" + "<div class='artist-title card-content'>" + "<h2 class='card-title'>" + artistName + "</h2>" + "</div>" + "</div>" + "</div>");
 
     $('#'+key).css('background-image', 'url(' + artistImg + ')');
   });
+});
+
+//=================================[ Modal Creation ]====================================//
+let createModal = function(key, artistName, artistBio, artistImg ) {
+    // console.log('name, artistBio, image', key, artistName, artistBio, artistImg);
+
+    let html = '<div class="modal fade"' + 'id="a' + key + '"' + 'tabindex="-1" role="dialog" aria-labelledby="' + artistName + '" aria-hidden="true">';
+    html += '<div class="modal-dialog" role="document">';
+    html += '<div class="modal-content">';
+    html += '<div class="modal-header">';
+    html += '<h5 class="modal-title"' + '>' + artistName + '</h5>';
+    html += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
+    html += '<span aria-hidden="true">&times;</span>';
+    html += '</button>';
+    html += '</div>';
+    html += '<div class="modal-body">';
+    html += artistBio;
+    html += '</div>';
+    html += '<div class="modal-footer">'; 
+    html += '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'; 
+    html += '</div>'; //footer
+    html += '</div>'; //content
+    html += '</div>'; //dialog
+    html += '</div>'; //modal
+    $('#artist-container').append(html);
+}
+
+//=================================[ Modal Listner ]====================================//
+$('#artist-container').on('click', '.artist-thumbnail', function(){
+    console.log('this', event.target.id);
+    $('#a' + event.target.id).modal({show:true});
 });
 
 })();
